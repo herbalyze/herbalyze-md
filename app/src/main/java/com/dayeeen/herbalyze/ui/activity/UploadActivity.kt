84a439cd8b31dd4146339a5e78ffaea7ff0b0ee7
dayeeen <dayensptr@gmail.com>
2024-06-17 17:13:58 +0700

    Menambahkan fitur ambil gambar melalui kamera dan gallery

--- app/src/main/java/com/dayeeen/herbalyze/ui/activity/UploadActivity.kt
+++ app/src/main/java/com/dayeeen/herbalyze/ui/activity/UploadActivity.kt
@@ -1,20 +1,75 @@
 package com.dayeeen.herbalyze.ui.activity
 
+import android.net.Uri
 import android.os.Bundle
-import androidx.activity.enableEdgeToEdge
+import android.util.Log
+import androidx.activity.result.PickVisualMediaRequest
+import androidx.activity.result.contract.ActivityResultContracts
 import androidx.appcompat.app.AppCompatActivity
 import com.dayeeen.herbalyze.R
 import com.dayeeen.herbalyze.databinding.ActivityUploadBinding
+import com.dayeeen.herbalyze.utils.getImageUri
+import com.dayeeen.herbalyze.utils.reduceFileImage
+import com.dayeeen.herbalyze.utils.uriToFile
 
 class UploadActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityUploadBinding
+    private var currentImageUri: Uri? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityUploadBinding.inflate(layoutInflater)
-        enableEdgeToEdge()
-        setContentView(R.layout.activity_upload)
+        setContentView(binding.root)
+
+        binding.galleryButton.setOnClickListener { startGallery() }
+        binding.cameraButton.setOnClickListener { startCamera() }
+        binding.uploadButton.setOnClickListener { uploadStory() }
 
     }
+
+    private fun startCamera() {
+        currentImageUri = getImageUri(this)
+        currentImageUri?.let { uri ->
+            launcherIntentCamera.launch(uri)
+        }
+    }
+
+    private val launcherIntentCamera = registerForActivityResult(
+        ActivityResultContracts.TakePicture()
+    ) { isSuccess ->
+        if (isSuccess) {
+            showImage()
+        }
+    }
+
+    private fun startGallery() {
+        launcherGallery.launch(PickVisualMediaRequest(ActivityResultContracts.PickVisualMedia.ImageOnly))
+    }
+
+    private val launcherGallery = registerForActivityResult(
+        ActivityResultContracts.PickVisualMedia()
+    ) { uri: Uri? ->
+        if (uri != null) {
+            currentImageUri = uri
+            showImage()
+        } else {
+            Log.d("Photo Picker", "No media selected")
+        }
+    }
+
+    private fun showImage() {
+        currentImageUri?.let {
+            Log.d("Image URI", "showImage: $it")
+            binding.thumbnail.setImageURI(it)
+        }
+    }
+
+    private fun uploadStory() {
+        currentImageUri?.let { uri ->
+            val imageFile = uriToFile(uri, this).reduceFileImage()
+            Log.d("Image File", "showImage: ${imageFile.path}")
+        }
+    }
+
 }
